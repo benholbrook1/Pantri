e839c8b24ae9b99720839c0e1c13a7b3d3779eb3
Ben <holbrookben12@gmail.com>
2025-07-05 23:07:25 -0400

    add notes to full links in item urls

--- src/inventory/urls.py
+++ src/inventory/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.item_list),
-    path('<id>/', views.item_detail),
+    path('', views.item_list),  ## full url = /api/items/
+    path('<id>/', views.item_detail),  ## full url = /api/items/<id>/
 ]

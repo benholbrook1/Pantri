9d73129fa1389e2b86df5e2376c00dc1d30b7942
Ben <holbrookben12@gmail.com>
2025-07-06 09:28:45 -0400

    convert item view to rest framework APIView

--- src/inventory/urls.py
+++ src/inventory/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.item_list),  ## full url = /api/items/
+    path('', views.ItemList.as_view()),  ## full url = /api/items/
     path('<id>/', views.item_detail),  ## full url = /api/items/<id>/
 ]
--- src/inventory/views.py
+++ src/inventory/views.py
@@ -3,18 +3,19 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from .models import Item
 from .serializers import ItemSerializer
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def item_list(request):
-    if request.method == 'GET':
+class ItemList(APIView):
+    def get(self, request):
         itemQueryset = Item.objects.all()
         serializer = ItemSerializer(itemQueryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
+    
+    def post(self, request):
         serializer = ItemSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.validated_data['created_by'] = request.user
